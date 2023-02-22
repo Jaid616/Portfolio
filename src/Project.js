2bfb550f7b448787fc96d8d8f08e75a2fa4a1537
Jaid616 <78507633+Jaid616@users.noreply.github.com>
2023-02-22 14:38:58 +0530

    Update Project.js
    Update new project

--- src/Project.js
+++ src/Project.js
@@ -12,6 +12,18 @@ export default function Project() {
               <h2 > All Projects</h2>
               <div className="underline"></div>
             </div>
+     <ProjectLeft 
+                sourceCode = "https://github.com/Jaid616/dashboard"
+                img = {IPTracker}
+                link = "https://dashboard-4khu.onrender.com/"
+                btn1= "React"
+                btn2 = "Material-UI"
+                btn3 = "Node"
+                number = "01" 
+               heading = "User Dashboard"
+                discription="In this project, user can register yourself. After Registration user can log in with own email id and password.
+                if the user want to change own password then user can request for change password after login."
+
                <ProjectRight 
                sourceCode = "https://github.com/Jaid616/Covid_Tracker"
                link="https://jaidnasim.000webhostapp.com/Covid_Tracker"
@@ -19,7 +31,7 @@ export default function Project() {
                btn2 = "JavaScript"
                btn3 = "BootStrap"
                img = {covidTracker}
-               number = "01" 
+               number = "02" 
                heading = "Covid-19 Tracker"
                 discription="A React App for all the latest updates about the spread of Corona Virus in India Features:- Data is analyzed with State wise statistics are shown, District wise statistics are shown, Updated regulary (All the available time stamps are notified), Resources List for almost every city is provided which includes TestLabs, Free Food Services, etc."/>
 
@@ -30,7 +42,7 @@ export default function Project() {
                 btn1= "HTML"
                 btn2 = "CSS"
                 btn3 = "JavaScript"
-                number = "02" 
+                number = "03" 
                heading = "IP Address Tracker"
                 discription="This method of tracking can have very broad results and can sometimes only get an accurate result with some input from the user about their location.In some countries laws are in place that state a service can only track you as far as your country without your consent. If the user consents then the service can view your ISP's logs and track you as accurately as possible"/>
 
